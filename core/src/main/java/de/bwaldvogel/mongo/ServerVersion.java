73ff1c835f565f86986804aa531017fcfae4ae25
jmarkerink <jmarkerink@outlook.com>
2026-05-26 19:20:10 +0200

    feat: added new Server/wire versions

--- core/src/main/java/de/bwaldvogel/mongo/ServerVersion.java
+++ core/src/main/java/de/bwaldvogel/mongo/ServerVersion.java
@@ -6,7 +6,12 @@ import java.util.List;
 public enum ServerVersion implements MongoVersion {
     MONGO_3_6(List.of(3, 6, 0), 6),
     MONGO_4_0(List.of(4, 0, 0), 7),
-    MONGO_5_0(List.of(5, 0, 0), 8);
+    MONGO_4_2(List.of(4, 2, 0), 8),
+    MONGO_4_4(List.of(4, 4, 0), 9),
+    MONGO_5_0(List.of(5, 0, 0), 13),
+    MONGO_5_1(List.of(5, 1, 0), 14),
+    MONGO_5_2(List.of(5, 2, 0), 15),
+    MONGO_5_3(List.of(5, 3, 0), 16);
 
     private final List<Integer> versionArray;
     private final int wireVersion;
--- core/src/test/java/de/bwaldvogel/mongo/ServerVersionTest.java
+++ core/src/test/java/de/bwaldvogel/mongo/ServerVersionTest.java
@@ -10,7 +10,12 @@ class ServerVersionTest {
     void testToVersionString() throws Exception {
         assertThat(ServerVersion.MONGO_3_6.toVersionString()).isEqualTo("3.6.0");
         assertThat(ServerVersion.MONGO_4_0.toVersionString()).isEqualTo("4.0.0");
+        assertThat(ServerVersion.MONGO_4_2.toVersionString()).isEqualTo("4.2.0");
+        assertThat(ServerVersion.MONGO_4_4.toVersionString()).isEqualTo("4.4.0");
         assertThat(ServerVersion.MONGO_5_0.toVersionString()).isEqualTo("5.0.0");
+        assertThat(ServerVersion.MONGO_5_1.toVersionString()).isEqualTo("5.1.0");
+        assertThat(ServerVersion.MONGO_5_2.toVersionString()).isEqualTo("5.2.0");
+        assertThat(ServerVersion.MONGO_5_3.toVersionString()).isEqualTo("5.3.0");
     }
 
 }
